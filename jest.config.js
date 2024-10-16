d5e8df1c822e90724dfba02b80d2f097ff754b40
obafemi oderanti <thevetdoctor@gmail.com>
2024-10-17 00:30:36 +0100

    ignore dist from test

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,6 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
